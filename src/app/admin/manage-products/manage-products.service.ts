f9257ba3bda56a0325587e74417a8dd32d77284c
Volodymyr Yas <volodymyr_yas@epam.com>
2024-05-21 23:47:45 +0300

    feat(task-7): add authorization

--- src/app/admin/manage-products/manage-products.service.ts
+++ src/app/admin/manage-products/manage-products.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { EMPTY, Observable } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { EMPTY, Observable, throwError, catchError } from 'rxjs';
 import { ApiService } from '../../core/api.service';
 import { switchMap } from 'rxjs/operators';
 
@@ -14,6 +15,7 @@ export class ManageProductsService extends ApiService {
     }
 
     return this.getPreSignedUrl(file.name).pipe(
+      catchError((error: unknown) => this.handleError(error)),
       switchMap((url) =>
         this.http.put(url, file, {
           headers: {
@@ -28,10 +30,42 @@ export class ManageProductsService extends ApiService {
   private getPreSignedUrl(fileName: string): Observable<string> {
     const url = this.getUrl('import', 'import');
 
+    const authToken: string | null = localStorage.getItem(
+      'authorization_token',
+    );
+
+    const headers: Record<string, string> = {};
+
+    if (authToken) {
+      headers.Authorization = `Basic ${localStorage.getItem('authorization_token')}`;
+    }
+
     return this.http.get<string>(url, {
+      headers,
       params: {
         name: fileName,
       },
     });
+  }
+
+  private handleError(error: unknown) {
+    const httpResponseError = error as HttpErrorResponse;
+
+    let message = '';
+
+    switch (httpResponseError.status) {
+      case 401:
+        message = 'Authorization header is not set';
+        break;
+      case 403:
+        message = 'Access denied, authorization header is not valid';
+        break;
+    }
+
+    if (message) {
+      alert(message);
+    }
+
+    return throwError(() => new Error(httpResponseError.message));
   }
 }
